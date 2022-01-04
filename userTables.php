75cc7fe2d2bd2a33962b825a4d00b461c3bbba3a
Sena Oz <tazeyta@gmail.com>
2022-01-05 00:54:38 +0300

    5 Jun 2022

--- userTables.php
+++ userTables.php
@@ -4,59 +4,15 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body style="padding: 5% 8%;">
-<?php include("menu.php"); include("db.php"); include("parameters.php");
+<?php include("menu.php"); include("parameters.php");
 if ($_SESSION['role'] == 'Professor') {
     header("Location: profTables.php"); }
 
 if (!isset($_SESSION["u_mail"])){
     header("Location: index.php");
 }
-$u_mail = $_SESSION["u_mail"];
-?>
-<h2 class="gradientText">My Courses</h2>
-<table id="tables">
-    <thead >
-    <tr>
-        <th>ID</th>
-        <th>Course Name</th>
-        <th>Course Description</th>
-        <th>Course Quota</th>
-        <th>Course Final Date</th>
-        <th>Credits</th>
-        <th>Consent</th>
-        <th>Course Professor</th>
-    </tr>
-    </thead>
-    <?php
-    $courses = $db -> query("SELECT `c_ID` FROM `courseDetails` WHERE `s_mail`='$u_mail'");
 
-    while ($row = $courses->fetch_row()) {
-        $c_id = $row[0];
-        $courseDetails = $db -> query("SELECT * FROM courses WHERE c_id='$c_id'");
-
-        while($rowC = $courseDetails->fetch_row()) {
-            $c_name = $rowC[1];
-            $c_description = $rowC[2];
-            $c_quota = $rowC[3];
-            $c_final_date = $rowC[4];
-            $c_credits = $rowC[5];
-            $c_consent = $rowC[6];
-            $c_professor = $rowC[7];
-            ?>
-            <tr>
-                <td><?php echo $c_id;  ?></td>
-                <td><?php echo $c_name;  ?></td>
-                <td><a id="edit" onclick="alert('<?php echo $c_description ?>')">Click to see</a></td>
-                <td><?php echo $c_quota;  ?></td>
-                <td><?php echo $c_final_date;  ?></td>
-                <td><?php echo $c_credits;  ?></td>
-                <td><?php echo $c_consent;  ?></td>
-                <td><?php echo $c_professor;  ?></td>
-            </tr> <?php
-        }
-    } ?>
-</table>
-<br><br>
+$role = $_SESSION["role"]; $u_mail = $_SESSION["u_mail"]; ?>
 <h2 class="gradientText">My Information</h2>
 <table id="tables">
     <thead>
@@ -85,6 +41,46 @@ $u_mail = $_SESSION["u_mail"];
             <td><?php echo $u_surname;  ?></td>
             <td><?php echo $u_phone;  ?></td>
             <td><?php echo $u_uni;  ?></td>
+        </tr> <?php
+    } ?>
+</table>
+<br><br>
+<h1 class="gradientText">My Courses</h1>
+<table id="tables">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Course Description</th>
+        <th>Final Date</th>
+        <th>Professor Name</th>
+        <th>Professor Mail</th>
+        <th>Credits</th>
+        <th>Grades</th>
+    </tr>
+    </thead>
+    <?php
+    $MyCourses = $db -> query("SELECT * FROM `ParticipantList` WHERE s_mail='$u_mail'");
+
+    while($rowC = $MyCourses->fetch_row()) {
+        $c_id = $rowC[1];
+        $c_name = $rowC[2];
+        $final_date = $rowC[3];
+        $prof_name = $rowC[5];
+        $prof_surname = $rowC[6];
+        $c_professor_mail = $rowC[4];
+        $c_credits = $rowC[7];
+        $c_description = $rowC[8];
+        ?>
+        <tr>
+            <td><?php echo $c_id;  ?></td>
+            <td><?php echo $c_name;  ?></td>
+            <td><a id="edit" onclick="alert('<?php echo $c_description ?>')">Click to see</a></td>
+            <td><?php echo $final_date;  ?></td>
+            <td><?php echo $prof_name." ".$prof_surname;  ?></td>
+            <td><?php echo $c_professor_mail;  ?></td>
+            <td><?php echo $c_credits;  ?></td>
+            <td>      </td>
         </tr> <?php
     } ?>
 </table>
