8e7dbc6c40f30fb379773afe2a90a554308a743c
Sena Oz <tazeyta@gmail.com>
2022-01-05 21:26:55 +0300

    5 Jun 22

--- deactivate.php
+++ deactivate.php
@@ -1,11 +1,5 @@
-<?php
-
-include("db.php");
-$mail = $_GET['d'];
-
+<?php include("db.php"); $mail = $_GET['d'];
 $sql = "UPDATE `users` SET `u_activate` = '0' WHERE `users`.`u_mail` = '$mail'";
 $result = $db->query($sql);
 unset($_SESSION["$mail"]);
-header("Location: adminTables.php");
-
-?>
+header("Location: adminTables.php"); ?>
